c7e239fc3bff9ce96ad8d33c4f500c6556a22ee3
thehaigo <sirannha@gmail.com>
2021-08-08 23:13:22 +0900

    bamboo using local adaper shpere local_root change to cwd

--- lib/basic_web/router.ex
+++ lib/basic_web/router.ex
@@ -75,6 +75,23 @@ defmodule BasicWeb.Router do
     delete "/*path_", ApiController, :index
   end
 
+  # Enables LiveDashboard only for development
+  #
+  # If you want to use the LiveDashboard in production, you should put
+  # it behind authentication and allow only admins to access it.
+  # If your application does not have an admins-only section yet,
+  # you can use Plug.BasicAuth to set up some basic authentication
+  # as long as you are also using SSL (which you should anyway).
+  if Mix.env() in [:dev, :test] do
+    import Phoenix.LiveDashboard.Router
+
+    scope "/" do
+      pipe_through :browser
+      live_dashboard "/dashboard", metrics: BasicWeb.Telemetry
+      forward "/sent_emails", Bamboo.SentEmailViewerPlug
+    end
+  end
+
   pipeline :sphere_browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -131,20 +148,4 @@ defmodule BasicWeb.Router do
   # scope "/api", BasicWeb do
   #   pipe_through :api
   # end
-
-  # Enables LiveDashboard only for development
-  #
-  # If you want to use the LiveDashboard in production, you should put
-  # it behind authentication and allow only admins to access it.
-  # If your application does not have an admins-only section yet,
-  # you can use Plug.BasicAuth to set up some basic authentication
-  # as long as you are also using SSL (which you should anyway).
-  if Mix.env() in [:dev, :test] do
-    import Phoenix.LiveDashboard.Router
-
-    scope "/" do
-      pipe_through :browser
-      live_dashboard "/dashboard", metrics: BasicWeb.Telemetry
-    end
-  end
 end

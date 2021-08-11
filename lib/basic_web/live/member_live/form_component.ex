61ec2bbc619262d344a921acbccee032aa607f81
piacerex <masq_wise@hotmail.com>
2021-08-11 21:23:16 +0900

    Refactor member image upload

--- lib/basic_web/live/member_live/form_component.ex
+++ lib/basic_web/live/member_live/form_component.ex
@@ -31,18 +31,16 @@ defmodule BasicWeb.MemberLive.FormComponent do
     save_member(socket, socket.assigns.action, member_params)
   end
 
-  defp save_member(socket, :edit, member_params) do
-    uploaded_files = save_upload_file(socket, member_params)
-    image = if uploaded_files == [] do  # In the case of [], the file is not specified
-      socket.assigns.member.image
-    else
-      if socket.assigns.member.image != nil do
-        path = Path.join(Application.fetch_env!(:sphere, :content_folder), socket.assigns.member.image)
-        File.rm(path)
-      end
-      List.last(uploaded_files)
+  def save_member(socket, :edit, params) do
+    image = case copy_upload_file(socket, params) do
+      []   -> socket.assigns.member.image  # In the case of [], the file is not choised
+      path -> 
+        if socket.assigns.member.image != nil do
+          File.rm(Path.join(Application.fetch_env!(:sphere, :content_folder), socket.assigns.member.image))
+        end
+        path
     end
-    case Members.update_member(socket.assigns.member, Map.put(member_params, "image", image)) do
+    case Members.update_member(socket.assigns.member, Map.put(params, "image", image)) do
       {:ok, _member} ->
         {:noreply,
         socket
@@ -50,18 +48,16 @@ defmodule BasicWeb.MemberLive.FormComponent do
         |> push_redirect(to: socket.assigns.return_to)}
 
       {:error, %Ecto.Changeset{} = changeset} ->
-        {:noreply, assign(socket, :changeset, changeset)}
+        {:noreply, assign(socket, changeset: changeset)}
     end
   end
 
-  defp save_member(socket, :new, member_params) do
-    uploaded_files = save_upload_file(socket, member_params)
-    image = if uploaded_files == [] do  # In the case of [], the file is not specified
-      ""
-    else
-      List.last(uploaded_files)
+  def save_member(socket, :new, params) do
+    image = case copy_upload_file(socket, params) do
+      []   -> socket.assigns.member.image  # In the case of [], the file is not choised
+      path -> path
     end
-    case Members.create_member(Map.put(member_params, "image", image)) do
+    case Members.create_member(Map.put(params, "image", image)) do
       {:ok, _member} ->
         {:noreply,
         socket
@@ -73,7 +69,7 @@ defmodule BasicWeb.MemberLive.FormComponent do
     end
   end
 
-  def save_upload_file(socket, member_params) do
+  def copy_upload_file(socket, params) do
     consume_uploaded_entries(socket, :avatar, fn %{path: path}, _entry ->
       type = ExImageInfo.seems?(File.read!(path)) |> Atom.to_string |> String.replace("jpeg", "jpg")
       now = NaiveDateTime.utc_now
@@ -81,10 +77,11 @@ defmodule BasicWeb.MemberLive.FormComponent do
         [
           Application.fetch_env!(:sphere, :content_folder), 
           "/images/data/", 
-          member_params["user_id"] <> "-" <> Date.to_string(now) <> "-" <> String.replace(Time.to_string(now), [":", "."], "") <> "." <> type
+          params["user_id"] <> "-" <> Date.to_string(now) <> "-" <> String.replace(Time.to_string(now), [":", "."], "") <> "." <> type
         ])
       File.cp!(path, dest)
       Routes.static_path(socket, "/images/data/#{Path.basename(dest)}")
     end)
+    |> List.last  # "Last" here means the first choised file
   end
 end

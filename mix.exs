67eecc709d2fc9e35da7268bd57a2ac1983657b2
rioorinoco <rio.orinoco0670@gmail.com>
2021-10-08 10:18:02 +0900

    modify_mix.exs

--- mix.exs
+++ mix.exs
@@ -52,13 +52,13 @@ defmodule Basic.MixProject do
 
       {:ex_image_info, "~> 0.2.4"},
 
-      {:phoenix, "~> 1.5"},
-      {:phoenix_ecto, "~> 4.4"},
-      {:ecto_sql, "~> 3.6"},
+      {:phoenix, "~> 1.5.9"},
+      {:phoenix_ecto, "~> 4.1"},
+      {:ecto_sql, "~> 3.4"},
       {:postgrex, ">= 0.0.0"},
-      {:phoenix_live_view, "~> 0.16"},
+      {:phoenix_live_view, "~> 0.15.1"},
       {:floki, ">= 0.30.0", only: :test},
-      {:phoenix_html, "~> 3.0"},
+      {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_dashboard, "~> 0.4"},
       {:telemetry_metrics, "~> 0.4"},

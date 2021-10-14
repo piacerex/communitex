6b32e5422ca1e82742e86d1f331ccf98419d7a9c
rioorinoco <rio.orinoco0670@gmail.com>
2021-10-14 14:56:23 +0900

    item_live-Add file upload and display imege files

--- lib/basic_web/live/item_live/form_component.ex
+++ lib/basic_web/live/item_live/form_component.ex
@@ -3,6 +3,8 @@ defmodule BasicWeb.ItemLive.FormComponent do
 
   alias Basic.Items
 
+  require ExImageInfo
+
   @impl true
   def update(%{item: item} = assigns, socket) do
     changeset = Items.change_item(item)
@@ -10,7 +12,9 @@ defmodule BasicWeb.ItemLive.FormComponent do
     {:ok,
      socket
      |> assign(assigns)
-     |> assign(:changeset, changeset)}
+     |> assign(:changeset, changeset)
+     |> assign(:uploaded_files, [])
+     |> allow_upload(:avatar, accept: :any, max_entries: 1)}
   end
 
   @impl true
@@ -28,28 +32,57 @@ defmodule BasicWeb.ItemLive.FormComponent do
   end
 
   defp save_item(socket, :edit, item_params) do
-    case Items.update_item(socket.assigns.item, item_params) do
+#    case Items.update_item(socket.assigns.item, item_params) do
+#      {:ok, _item} ->
+#        {:noreply,
+#         socket
+#         |> put_flash(:info, "Item updated successfully")
+#         |> push_redirect(to: socket.assigns.return_to)}
+#
+#      {:error, %Ecto.Changeset{} = changeset} ->
+#        {:noreply, assign(socket, :changeset, changeset)}
+#    end
+    image = binary_upload_file(socket)
+    case Items.update_item(socket.assigns.item, Map.put(item_params, "image", image)) do
       {:ok, _item} ->
         {:noreply,
-         socket
-         |> put_flash(:info, "Item updated successfully")
-         |> push_redirect(to: socket.assigns.return_to)}
-
-      {:error, %Ecto.Changeset{} = changeset} ->
-        {:noreply, assign(socket, :changeset, changeset)}
-    end
-  end
-
-  defp save_item(socket, :new, item_params) do
-    case Items.create_item(item_params) do
-      {:ok, _item} ->
-        {:noreply,
-         socket
-         |> put_flash(:info, "Item created successfully")
-         |> push_redirect(to: socket.assigns.return_to)}
+        socket
+        |> push_redirect(to: socket.assigns.return_to)}
 
       {:error, %Ecto.Changeset{} = changeset} ->
         {:noreply, assign(socket, changeset: changeset)}
     end
+  end
+
+  defp save_item(socket, :new, item_params) do
+#    case Items.create_item(item_params) do
+#      {:ok, _item} ->
+#        {:noreply,
+#         socket
+#         |> put_flash(:info, "Item created successfully")
+#         |> push_redirect(to: socket.assigns.return_to)}
+#
+#      {:error, %Ecto.Changeset{} = changeset} ->
+#        {:noreply, assign(socket, changeset: changeset)}
+#    end
+    image = binary_upload_file(socket)
+    case Items.create_item(Map.put(item_params, "image", image)) do
+      {:ok, _item} ->
+        {:noreply,
+        socket
+        |> push_redirect(to: socket.assigns.return_to)}
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        {:noreply, assign(socket, changeset: changeset)}
+    end
+  end
+
+  def binary_upload_file(socket) do
+    consume_uploaded_entries(socket, :avatar, fn %{path: path}, _entry -> File.read!(path) end)
+    |> List.last  # "Last" here means the first choised file
+    |> case do
+         nil    -> nil
+         binary -> Base.encode64(binary)
+       end
   end
 end

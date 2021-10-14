5a11a745bb6ab92dfc638c347a2b94e35a5a1563
rioorinoco <rio.orinoco0670@gmail.com>
2021-10-14 14:01:15 +0900

    Feature ui_live/item_live

---
+++ lib/basic_web/ui_live/item_live/show.ex
@@ -0,0 +1,21 @@
+defmodule BasicWeb.ItemUiLive.Show do
+  use BasicWeb, :live_view
+
+  alias Basic.Items
+
+  @impl true
+  def mount(_params, _session, socket) do
+    {:ok, socket}
+  end
+
+  @impl true
+  def handle_params(%{"id" => id}, _, socket) do
+    {:noreply,
+     socket
+     |> assign(:page_title, page_title(socket.assigns.live_action))
+     |> assign(:item, Items.get_item!(id))}
+  end
+
+  defp page_title(:show), do: "Show Item"
+  defp page_title(:edit), do: "Edit Item"
+end

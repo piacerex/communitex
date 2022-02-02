cfb9c84df5370ff1171701393d3e7be0eaf2d0dd
rioorinoco <rio.orinoco0670@gmail.com>
2022-02-02 10:34:25 +0900

    install tailwind

--- config/dev.exs
+++ config/dev.exs
@@ -24,6 +24,7 @@ config :basic, BasicWeb.Endpoint,
   debug_errors: true,
   secret_key_base: "p3Q+79AYsBUlq2hvi6HobptFQksQvbuLThWYKscdCiWtiJgvG2WHqzoXsbcmFqn7",
   watchers: [
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]},
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
     esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
   ]
